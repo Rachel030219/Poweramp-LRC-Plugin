a356fe3333d6de8905d231c3b7c87ebda4090c63
Rachel030219 <tangrui003@gmail.com>
2020-03-16 23:09:58 +0800

    modified UI

--- app/src/main/java/net/rachel030219/poweramplrc/PermissionActivity.kt
+++ app/src/main/java/net/rachel030219/poweramplrc/PermissionActivity.kt
@@ -61,7 +61,6 @@ class PermissionActivity: AppCompatActivity() {
             floating_asked = false
             if (Settings.canDrawOverlays(this)) {
                 permission_floating_text.setText(R.string.permission_floating_granted)
-                permission_floating_text.setTextColor(getColor(R.color.text_success))
                 permission_floating_check.visibility = View.GONE
                 floating = true
             } else {
@@ -76,7 +75,6 @@ class PermissionActivity: AppCompatActivity() {
         if (requestCode == REQUEST_PERMISSION) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 permission_storage_text.setText(R.string.permission_storage_granted)
-                permission_storage_text.setTextColor(getColor(R.color.text_success))
                 permission_storage_check.visibility = View.GONE
                 storage = true
             } else {
