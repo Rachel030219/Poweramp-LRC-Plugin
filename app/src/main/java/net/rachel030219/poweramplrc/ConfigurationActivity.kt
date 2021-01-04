1428750f1dc249bdd311fae800dd4009dbf074f2
Rachel030219 <291054446@qq.com>
2021-01-04 11:14:58 +0800

    removed automatic wake when initialized

--- app/src/main/java/net/rachel030219/poweramplrc/ConfigurationActivity.kt
+++ app/src/main/java/net/rachel030219/poweramplrc/ConfigurationActivity.kt
@@ -13,24 +13,26 @@ class ConfigurationActivity: AppCompatActivity() {
         setContentView(R.layout.activity_configuration)
 
         // send notification
-        val statusIntent = registerReceiver(null, IntentFilter(PowerampAPI.ACTION_STATUS_CHANGED))
-        val trackIntent = registerReceiver(null, IntentFilter(PowerampAPI.ACTION_TRACK_CHANGED))
-        if (statusIntent != null && trackIntent != null) {
-            val bundle = trackIntent.getBundleExtra(PowerampAPI.TRACK)?.apply {
-                putBoolean(PowerampAPI.PAUSED, statusIntent.getBooleanExtra(PowerampAPI.PAUSED, true))
-                putInt(PowerampAPI.Track.POSITION, statusIntent.getIntExtra(PowerampAPI.Track.POSITION, -1))
-            }
-            bundle?.also {
-                if (LrcWindow.displaying)
-                    LrcWindow.sendNotification(this, it, true)
-                else
-                    LrcWindow.sendNotification(this, it, false)
+        if (!LrcWindow.initialized) {
+            val statusIntent = registerReceiver(null, IntentFilter(PowerampAPI.ACTION_STATUS_CHANGED))
+            val trackIntent = registerReceiver(null, IntentFilter(PowerampAPI.ACTION_TRACK_CHANGED))
+            if (statusIntent != null && trackIntent != null) {
+                val bundle = trackIntent.getBundleExtra(PowerampAPI.TRACK)?.apply {
+                    putBoolean(PowerampAPI.PAUSED, statusIntent.getBooleanExtra(PowerampAPI.PAUSED, true))
+                    putInt(PowerampAPI.Track.POSITION, statusIntent.getIntExtra(PowerampAPI.Track.POSITION, -1))
+                }
+                bundle?.also {
+                    if (LrcWindow.displaying)
+                        LrcWindow.sendNotification(this, it, true)
+                    else
+                        LrcWindow.sendNotification(this, it, false)
 
-                val intents = Intent(this, LrcService::class.java).putExtra("request", LrcWindow.REQUEST_UPDATE).putExtras(it)
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-                    startForegroundService(intents.apply { putExtra("foreground", true) })
-                else
-                    startService(intents)
+                    val intents = Intent(this, LrcService::class.java).putExtra("request", LrcWindow.REQUEST_UPDATE).putExtras(it)
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                        startForegroundService(intents.apply { putExtra("foreground", true) })
+                    else
+                        startService(intents)
+                }
             }
         }
     }
